957b86f72f8f48540e6063c851a5548db4672446
jake-egan <jakeegan0203@gmail.com>
2024-12-15 03:10:59 +0000

    Added most info

---
+++ Js/scripts.js
@@ -0,0 +1,12 @@
+function getBirthday(Birthdate) {
+    const birthday = new Date(Birthdate);
+    const diff = Date.now() - birthday.getTime();
+    const age = Math.floor(diff / (365.25 * 24 * 60 * 60 * 1000));
+    return age;
+}
+
+
+  const Birthdate = '2003-02-03';
+  const age = getBirthday(Birthdate);
+
+  document.getElementById('age').textContent = `DOB: ${Birthdate} | Age: ${age}`;
